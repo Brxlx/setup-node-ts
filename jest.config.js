bebd920566f41da9d3e5701ec8fffea2645a5b47
Bruno Santos <brcesar19@gmail.com>
2020-07-30 15:42:24 -0300

    Melhorando array de testMatch

--- jest.config.js
+++ jest.config.js
@@ -144,7 +144,10 @@ module.exports = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+  testMatch: [
+    '**/__tests__/**/*.ts?(x)', 
+    '**/?(*.)+(spec|test).ts?(x)'
+],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
